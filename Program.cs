fe1e52b6f600ae1774ec4c533279290ad29b2fc4
Fórizs Márk <112852700+GrumpyMonk6996@users.noreply.github.com>
2022-09-07 08:47:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kereget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem adja meg a nullát.");
+            try
+            {
+                double nulla;
+                do
+                {
+                    nulla = double.Parse(Console.ReadLine());
+                    if (nulla!=0)
+                    {
+                        Console.WriteLine("A szám nem jó");
+                    }
+                    else
+                    {
+                        Console.WriteLine("elég");
+                    }
+                } while (nulla != 0);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Számot adj meg");
+            }
+            Console.ReadKey();
+        }
+    }
+}
